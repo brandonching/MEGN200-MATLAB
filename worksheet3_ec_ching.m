c527dfc55f38953e64607bce5fd3abf69f6dde0e
Brandon Ching <68457375+brandonching@users.noreply.github.com>
2021-09-09 11:38:39 -0600

    WS3

---
+++ worksheet3_ec_ching.m
@@ -0,0 +1,19 @@
+% MEGN200: Worksheet 3 EC
+% Section - C
+% Brandon Ching
+% 9/6/2021 and Version 1
+
+%% Extra Credit 1
+
+clc; clear;
+close all;
+
+[num,txt,raw]=xlsread('HW_Grades');
+for idx = [1:3]
+    student = txt(idx+1);
+end
+
+%% Extra Credit 2
+
+clc; clear;
+close all;
