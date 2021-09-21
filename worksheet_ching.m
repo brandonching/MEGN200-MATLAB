a1110d41774662425c615dfe87ffb9481693b1eb
Brandon Ching <bching9@gmail.com>
2021-09-21 01:32:02 -0600

    WS 5

---
+++ worksheet_ching.m
@@ -0,0 +1,28 @@
+% MEGN200: Worksheet 
+% Section - C
+% Brandon Ching
+% 9/21/2021 and Version 1
+
+%% Tuesday Problem 1
+clc; clear;
+close all;
+
+%% Tuesday Problem 2
+clc; clear;
+close all;
+
+%% Tuesday Problem 3
+clc; clear;
+close all;
+
+%% Thursday Problem 1
+clc; clear;
+close all;
+
+%% Thursday Problem 2
+clc; clear;
+close all;
+
+%% Thursday Problem 3
+clc; clear;
+close all;
