b72a543ae22ad2c2bbf82441e72e01b970708bd2
Brandon Ching <bching9@gmail.com>
2021-10-03 14:37:47 -0600

    Complete Part 1

--- Projectile Motion with Drag/projectile_motion_drag.m
+++ Projectile Motion with Drag/projectile_motion_drag.m
@@ -24,12 +24,12 @@ time_guess=0:delta_t:100;   % create an initial time guess from 0 to 100s (arbit
 t=[0];    % create actual flight time vector starting with the initial time value as the loop iterates
 D = 1/2*(rho * cd * A); % calculate drag constant based on cd, row, A
 
-for n = 1
+for n = 1:length(time_guess)-1
     t(n+1) = t(n) + delta_t;    % store the current time value into the actual flight time array t
-    v(n) = sqrt(vy(n)^2+vx(n)^2); % calculate Eq(1) velocity magnitude v(n)
+    v(n+1) = sqrt(vy(n)^2+vx(n)^2); % calculate Eq(1) velocity magnitude v(n)
     ay(n+1) = -g-(D/m) * v(n) *vy(n); % calculate Eq(2) ay(n+1)
     vy(n+1) = vy(n) + ay(n) * delta_t; % calculate Eq(3) vy(n+1)
-    y(n+1) = y(n) + vy(n) * delta_t + 1/2*ay(n)+delta_t^2; % calculate Eq(4) y(n+1)
+    y(n+1) = y(n) + vy(n) * delta_t + 1/2*ay(n)*delta_t^2; % calculate Eq(4) y(n+1)
     ax(n+1) = -(D/m)*v(n)*vx(n); % calculate Eq(5) ax(n+1)
     vx(n+1) = vx(n)+ax(n)*delta_t; % calculate Eq(6) vx(n+1)
     x(n+1) = x(n) +vx(n) * delta_t + 1/2*ax(n) * delta_t^2; % calculate Eq(7) x(n+1)
